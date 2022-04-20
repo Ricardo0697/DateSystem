ae5b7c560c078a0f0720c556948161f48746d951
Ricardo0697 <37809835+Ricardo0697@users.noreply.github.com>
2022-04-19 22:23:52 -0600

    Update User.js

--- app/Models/User.js
+++ app/Models/User.js
@@ -34,6 +34,9 @@ class User extends Model {
   tokens () {
     return this.hasMany('App/Models/Token')
   }
+  quote () {
+    return this.hasMany('App/Models/Quote')
+  }
 }
 
 module.exports = User
